a96bc1edea4a46390fab308c736cd602441ef5bb
srappaport <sarah.rappaport@greendot.org>
2018-07-10 18:29:44 +0000

    automatically backed up by learn

---
+++ demo.rb
@@ -0,0 +1,5 @@
+loop do
+  puts "Watch me whip"
+  puts "Watch me nene"
+  break
+end
--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,16 @@
 # Build your say_hello method here
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+say_hello
+say_hello("Colin")
+
+
+def Fizzbuzz(num)
+  #code
+end
+  Fizzbuzz(5)==Buzz
+  Fizzbuzz(3)==Fizz
+  Fizzbuzz(15)==Fizzbuzz
+  
